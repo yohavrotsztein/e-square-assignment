edf083c6454614973fb9baa3c46838a08483833c
Yohav <yohavrotsztein@gmail.com>
2022-12-26 23:04:31 +0200

    last commit

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -1,8 +1,9 @@
-import { Card, CardHeader, Avatar, CardActions, IconButton, Box, Tooltip } from '@mui/material';
+import { Card, CardHeader, Avatar, CardActions, IconButton, Box, Tooltip, Typography } from '@mui/material';
 import { Icon } from '@iconify/react';
 import { removeWishlist } from '../redux/books.js';
 import { useDispatch } from '../redux/store';
 import DialogDetails from './DialogDetails';
+import '../index.css';
 
 
 export default function BookCard(props) {
@@ -10,11 +11,12 @@ export default function BookCard(props) {
   const dispatch = useDispatch();
   const { title, imageLinks } = props.book.volumeInfo;
 
+
   return (
-    <Card>
+    <Card >
       <CardHeader
         avatar={<Avatar sx={{ width: 72, height: 72, boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px' }} aria-label="recipe" src={imageLinks?.smallThumbnail || ''} />}
-        title={`${title}`}
+        title={<Tooltip title={title}><Typography noWrap >{title}</Typography></Tooltip>}
       />
       <CardActions disableSpacing sx={{ display: "flex", justifyContent: "flex-end" }}>
         {props.type === 'wishlist' ?
